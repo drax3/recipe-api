bcbf5b5a6b541bb39d3a0316caf440f3e68015e6
Tushar <tsompura0006@gmail.com>
2025-07-18 17:09:08 +0530

    database setup

--- install_deps.sh
+++ install_deps.sh
@@ -2,17 +2,28 @@
 
 set -e
 
-apk add --no-cache gcc musl-dev libffi-dev openssl-dev python3-dev cargo
+# Install dependencies
+apk add --update --no-cache postgresql-client && \
+apk add --update --no-cache --virtual .tmp-build-deps \
+  build-base postgresql-dev musl-dev
+
+# Upgrade pip and install pipenv
 pip install --upgrade pip pipenv
 
-pipenv lock
-
+# Ensure pipenv uses the system Python
 pipenv --python $(which python3)
 
+# Lock dependencies
+pipenv lock
+
+# Install dependencies based on DEV flag
 if [ "$DEV" = "true" ]; then
   pipenv install --dev --deploy --system
 else
   pipenv install --deploy --system
 fi
 
+# Cleanup
+rm -rf /tmp && \
+apk del .tmp-build-deps && \
 adduser --disabled-password --no-create-home django-user
